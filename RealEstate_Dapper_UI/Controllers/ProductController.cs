8d25dbf48239293b0324dcc59248c856f52de4ac
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-09-19 21:10:50 +0300

    Ürün Listesi Api Consume İşlemi

---
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.ProductDTOs;
+using System.Text;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDTO createProductDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createProductDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7067/api/Products", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ RealEstate_Dapper_UI/DTOs/ProductDTOs/UpdateProductDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace RealEstate_Dapper_UI.DTOs.ProductDTOs
+{
+    public class UpdateProductDTO
+    {
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string CategoryName { get; set; }
+        public string CoverImage { get; set; }
+        public string Type { get; set; }
+        public string Address { get; set; }
+    }
+}
