a8eb3f362c8160912c9cd68f3b18d5515ba978d9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-10-04 16:27:25 +0300

    Employee İşlemleri

--- RealEstate_Dapper_Api/Controllers/EmployeeController.cs
+++ RealEstate_Dapper_Api/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_API.DTOs.EmployeeDTOs;
+using RealEstate_Dapper_API.Repositories.EmployeeRepository;
 
 namespace RealEstate_Dapper_API.Controllers
 {
@@ -7,5 +8,46 @@ namespace RealEstate_Dapper_API.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EmployeeList()
+        {
+            var values = await _employeeRepository.GetAllEmployeeAsync();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateEmployee(CreateEmployeeDTO createEmployeeDTO)
+        {
+            _employeeRepository.CreateEmployee(createEmployeeDTO);
+            return Ok("Personel Başarılı Bir Şekilde Eklendi");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            _employeeRepository.DeleteEmployee(id);
+            return Ok("Personel Başarılı Bir Şekilde Silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDTO updateEmployeeDTO)
+        {
+            _employeeRepository.UpdateEmployee(updateEmployeeDTO);
+            return Ok("Personel Başarıyla Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployee(int id)
+        {
+            var value = await _employeeRepository.GetEmployee(id);
+            return Ok(value);
+        }
     }
 }
---
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.EmployeeDTOs;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public EmployeeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Employee");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
