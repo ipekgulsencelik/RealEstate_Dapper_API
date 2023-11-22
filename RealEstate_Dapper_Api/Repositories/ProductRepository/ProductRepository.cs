e67f08f521516871d0eb9fab02fa78fc949fbcbc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-22 12:07:11 +0300

    API Üzerinde Günün İlanı Seçme ve Kaldırma

--- RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
+++ RealEstate_Dapper_Api/Repositories/ProductRepository/ProductRepository.cs
@@ -32,5 +32,27 @@ namespace RealEstate_Dapper_Api.Repositories.ProductRepository
                 return values.ToList();
             }
         }
+
+        public async void ProductDealOfTheDayStatusChangeToFalse(int id)
+        {
+            string query = "Update Products Set DealOfTheDay=0 Where ProductID=@productID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@productID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async void ProductDealOfTheDayStatusChangeToTrue(int id)
+        {
+            string query = "Update Products Set DealOfTheDay=1 Where ProductID=@productID";
+            var parameters = new DynamicParameters();
+            parameters.Add("@productID", id);
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
     }
 }
