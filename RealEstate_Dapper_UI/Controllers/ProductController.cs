0b749fc625a5dc5ee144bf3d9ca3d2fe983aca86
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-11-28 12:08:58 +0300

    Dashboard Hazırlığı

--- RealEstate_Dapper_UI/Controllers/ProductController.cs
+++ RealEstate_Dapper_UI/Controllers/ProductController.cs
@@ -62,5 +62,27 @@ namespace RealEstate_Dapper_UI.Controllers
             }
             return View();
         }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToFalse(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Products/ProductDealOfTheDayStatusChangeToFalse/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToTrue(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/Products/ProductDealOfTheDayStatusChangeToTrue/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
