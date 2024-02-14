ce52315a5cb55f7d2405583cf2691719d870a7f4
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-02-14 15:16:55 +0300

    Estate Agent Paneli

---
+++ RealEstate_Dapper_Api/Controllers/TokenCreateController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_API.Tools;
+
+namespace RealEstate_Dapper_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenCreateController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateToken(GetCheckAppUserViewModel model)
+        {
+            var values = JwtTokenGenerator.GenerateToken(model);
+            return Ok(values);
+        }
+    }
+}
