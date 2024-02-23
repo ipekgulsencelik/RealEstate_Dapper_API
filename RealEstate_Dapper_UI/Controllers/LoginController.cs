c4fe512cd0ef26b708a109b3aa29bb472c88873e
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-02-23 16:31:49 +0300

    Token'ın UI Kısmında Login ile Kullanılması

---
+++ RealEstate_Dapper_UI/Controllers/LoginController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_UI.DTOs.LoginDTOs;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text;
+using RealEstate_Dapper_UI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+	public class LoginController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		
+		public LoginController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateLoginDTO createLoginDTO)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var content = new StringContent(JsonSerializer.Serialize(createLoginDTO), Encoding.UTF8, "application/json");
+			var response = await client.PostAsync("https://localhost:7067/api/Login", content);
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonData = await response.Content.ReadAsStringAsync();
+				var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+				{
+					PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+				});
+
+				if (tokenModel != null)
+				{
+					JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+					var token = handler.ReadJwtToken(tokenModel.Token);
+					var claims = token.Claims.ToList();
+
+					if (tokenModel.Token != null)
+					{
+						claims.Add(new Claim("realestatetoken", tokenModel.Token));
+						var claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+						var authProps = new AuthenticationProperties
+						{
+							ExpiresUtc = tokenModel.ExpireDate,
+							IsPersistent = true
+						};
+
+						await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProps);
+						return RedirectToAction("Index", "MyAdverts");
+					}
+				}
+			}
+			return View();
+		}
+	}
+}
---
+++ RealEstate_Dapper_UI/DTOs/LoginDTOs/CreateLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RealEstate_Dapper_UI.DTOs.LoginDTOs
+{
+	public class CreateLoginDTO
+	{
+		public string? Username { get; set; }
+		public string? Password { get; set; }
+	}
+}
