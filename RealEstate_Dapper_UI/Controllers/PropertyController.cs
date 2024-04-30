c857c644c82bc8640bd3924c0efcf8966b809f49
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-04-30 17:14:38 +0300

    Single Property Sayfası

---
+++ RealEstate_Dapper_UI/Controllers/PropertyController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.DTOs.PopularLocationDTOs;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PropertyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7067/api/PopularLocation");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultPopularLocationDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PropertySingle(int id)
+        {
+            return View();
+        }
+    }
+}
